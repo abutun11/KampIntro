a54f1dd0769f97d8b99b2e3254c41b2808ac22eb
abutun11 <butunalican01@gmail.com>
2023-09-07 11:59:52 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Porgramlamaya Başalngıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Porgramlamaya Başalngıç İçin Temel Kurs", "Java", "Python" , "C#" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");          
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
